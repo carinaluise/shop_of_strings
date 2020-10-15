67038b6ccd2f0adbc617d7efa6118a00a1499402
Carina <carinalrobinson@gmail.com>
2020-10-15 12:25:45 +0200

    changed card details

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -51,7 +51,7 @@ return (
     <WarningContainer>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 12/20 - CVV: 123
     </WarningContainer>
     <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
